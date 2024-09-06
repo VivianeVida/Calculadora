23a2efa075b98d98fb0a1fe6738607b3ae295734
Viviane Vida <67081266+VivianeVida@users.noreply.github.com>
2024-09-05 22:09:01 -0300

    Update calculadora2.py

--- calculadora2.py
+++ calculadora2.py
@@ -1,13 +1,3 @@
-*/Faça uma função calculadora que os números e as operações serão feitas pelo usuário. O código deve ficar rodando infinitamente até que o usuário escolha a opção de sair. No início, o programa mostrará a seguinte lista de operações:
-
-1: Soma
-2: Subtração
-3: Multiplicação
-4: Divisão
-0: Sair
-
-Digite o número para a operação correspondente e caso o usuário introduza qualquer outro, o sistema deve mostrar a mensagem “Essa opção não existe” e voltar ao menu de opções.*/
-
 def calculadora():
     while True:
         print("\nEscolha a operação:")
